a19646dde7f25849039ca21a1e1f637cbe8abcdf
Health Information Exchange Development <46489004+HIE-DEV@users.noreply.github.com>
2019-01-15 16:00:03 +0000

    Add files via upload

--- autoinstall-hapi-fhir-server.sh
+++ autoinstall-hapi-fhir-server.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#v1.1
+
 #Do a sudo/root level check
 uid=$(id -u)
 
@@ -11,24 +14,24 @@ read -e -p "Please specify port not in use by any other service to open the HAPI
 read -e -p "If your system uses firewalld (AKA firewall-cmd), would you like me to automatically open the port and reload the firewall? y/n: " -i "n" openfirewall
 
 cd "$location"
+rm "./hapi-fhir-$version-cli.zip"
 wget "https://github.com/jamesagnew/hapi-fhir/releases/download/v$version/hapi-fhir-$version-cli.zip"
 unzip "hapi-fhir-$version-cli.zip"
+
+localip=$(hostname -I)
+
+if [ "$openfirewall" == "y" ]; then
+ 	firewall-cmd "--permanent" "--add-port=$port/tcp"
+	firewall-cmd "--reload"
+	echo "Firewall updated with port $port"
+fi
 
 #Delete a pre-existing start script if it exists
 file="./start-hapi-fhir-server.sh"
 [ -e file ] && rm file
 
 #Write an updated version with the new settings
-echo "nohup java -jar hapi-fhir-cli.jar run-server -v $stuversion -p $port --allow-external-refs &" > ./start-hapi-fhir-server.sh
+echo "nohup \"java -jar hapi-fhir-cli.jar run-server -v $stuversion -p $port --allow-external-refs\" &" > ./start-hapi-fhir-server.sh
 
-nohup "bash ./start-hapi-fhir-server.sh &"
-
-localip=$(hostname -I)
-
-if [ "$openfirewall" == "y" ]; then
- 	firewall-cmd "--permanent --add-port=$port/tcp"
-	firewall-cmd "--reload"
-	echo "Firewall updated with port $port"
-fi
-
+nohup "bash ./start-hapi-fhir-server.sh" & 
 echo "This may appear as an error, but this script's best guess for the local URL for your HAPI FHIR server is at: http://$localip:$port"
