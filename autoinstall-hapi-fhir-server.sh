634027c3ee9b52aab2d1822417dfd343d64a06aa
Health Information Exchange Development <46489004+HIE-DEV@users.noreply.github.com>
2019-01-15 14:50:54 +0000

    Add files via upload

--- autoinstall-hapi-fhir-server.sh
+++ autoinstall-hapi-fhir-server.sh
@@ -4,19 +4,11 @@ uid=$(id -u)
 #We don't have root permissions, so we can't install or upgrade anything, so we quit
 [ $uid -ne 0 ] && { echo "Requires root (sudo) to successfully run this script."; exit 1; }
 
-read -e -p "Please specify HAPI FHIR version, EG 3.6.0:" -i "3.6.0" version
-
-echo "Please specify STU/DSTU version in all lowercase, EG dstu3"
-read stuversion
-
-echo "Please specify install location without end slash. EG /opt"
-read location
-
-echo "Please specify port not in use by any other service to open the HAPI FHIR server on, EG 8090"
-read port
-
-echo "If your system uses firewalld (AKA firewall-cmd), would you like me to automatically open the port and reload the firewall? y/n"
-read openfirewall
+read -e -p "Please specify HAPI FHIR version, EG 3.6.0: " -i "3.6.0" version
+read -e -p "Please specify STU/DSTU version in all lowercase, EG dstu3: " -i "dtu3" stuversion
+read -e -p "Please specify install location without end slash. EG /opt" -i "/opt" location
+read -e -p "Please specify port not in use by any other service to open the HAPI FHIR server on, EG 8090: " -i "8090" port
+read -e -p "If your system uses firewalld (AKA firewall-cmd), would you like me to automatically open the port and reload the firewall? y/n: " -i "n" openfirewall
 
 cd "$location"
 wget "https://github.com/jamesagnew/hapi-fhir/releases/download/v$version/hapi-fhir-$version-cli.zip"
@@ -25,7 +17,8 @@ java -jar hapi-fhir-cli.jar run-server
 nohup "java -jar hapi-fhir-cli.jar run-server -v $stuversion -p $port --allow-external-refs &"
 localip=$(hostname -I)
 
-if [ "$openfirewall" == "y" ]; then
+if [ "$
+" == "y" ]; then
  	firewall-cmd "--permanent --add-port=$port/tcp"
 	firewall-cmd "reload"
 	echo "Firewall updated with port $port"
