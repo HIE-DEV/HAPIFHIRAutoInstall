ae504636806299b08cb6c224f4c9d42db13c7315
Health Information Exchange Development <46489004+HIE-DEV@users.noreply.github.com>
2019-01-16 12:09:23 +0000

    Add files via upload

--- autoinstall-hapi-fhir-server.sh
+++ autoinstall-hapi-fhir-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "HAPI FHIR AutoInstaller Version 1.2"
+echo "HAPI FHIR AutoInstaller Version 1.3"
 
 #Do a sudo/root level check
 uid=$(id -u)
@@ -10,7 +10,7 @@ uid=$(id -u)
 
 read -e -p "Please specify HAPI FHIR version, EG 3.6.0: " -i "3.6.0" version
 read -e -p "Please specify STU/DSTU version in all lowercase, EG dstu3: " -i "dstu3" stuversion
-read -e -p "Please specify install location without end slash. EG /opt: " -i "/opt" location
+read -e -p "Please specify install location without end slash. If you want root, leave blank. EG /opt: " -i "/opt" location
 read -e -p "Please specify port not in use by any other service to open the HAPI FHIR server on, EG 8090: " -i "8090" port
 read -e -p "If your system uses firewalld (AKA firewall-cmd), would you like me to automatically open the port and reload the firewall? y/n: " -i "n" openfirewall
 
@@ -29,7 +29,7 @@ localip=$(hostname -I)
 localip_notrailspace="$(echo -e "${localip}" | sed -e 's/[[:space:]]*$//')"
 
 if [ "$openfirewall" == "y" ]; then
- 	firewall-cmd "--permanent" "--add-port=$port/tcp"
+	firewall-cmd "--permanent" "--add-port=$port/tcp"
 	firewall-cmd "--reload"
 	echo "Firewall updated with port $port"
 fi
@@ -40,10 +40,20 @@ file="$location/start-hapi-fhir-server.sh"
 
 echo "Generating startup script at $location/start-hapi-fhir-server.sh"
 #Write an updated version with the new settings
-echo "java -jar $location/hapi-fhir-cli.jar run-server -v $stuversion -p $port --allow-external-refs" > "$location/start-hapi-fhir-server.sh"
 
-echo "This may appear as an error, but this script's best guess for the local URL for your HAPI FHIR server is at: http://$localip_notrailspace:$port"
+#The search for a process with grep itself includes grep as a process, so we must remove it
+#
+
+java_cmd="java -jar $location/hapi-fhir-cli.jar run-server -v $stuversion -p $port --allow-external-refs"
+nohup_cmd="nohup $java_cmd"
+echo_cmd="echo \"Process already started\""
+check_if_running="$(ps -aux | grep -v \"grep\" | grep \"java -jar $location/hapi-fhir-cli.jar run-server -v dstu3 -p 8090 --allow-external-refs\")"
+output_cmd="if [\"\" != \"$check_if_running\"]; then\n\t$nohup_cmd\nelse\n\techo_cmd\nfi"
+
+echo "$output_cmd"> "$location/start-hapi-fhir-server.sh"
 
 bash "$location/start-hapi-fhir-server.sh"
+
+echo "This may appear as an error, but this script's best guess for the local URL for your HAPI FHIR server is at: http://$localip_notrailspace:$port"
 
 echo "Script for starting the server can be found at $location/start-hapi-fhir-server.sh"
