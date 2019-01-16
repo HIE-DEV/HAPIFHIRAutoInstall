8128671aa6b013046c8ee2a307b1038fe2670ba2
Health Information Exchange Development <46489004+HIE-DEV@users.noreply.github.com>
2019-01-16 12:12:27 +0000

    Add files via upload

--- autoinstall-hapi-fhir-server.sh
+++ autoinstall-hapi-fhir-server.sh
@@ -12,7 +12,7 @@ read -e -p "Please specify HAPI FHIR version, EG 3.6.0: " -i "3.6.0" version
 read -e -p "Please specify STU/DSTU version in all lowercase, EG dstu3: " -i "dstu3" stuversion
 read -e -p "Please specify install location without end slash. If you want root, leave blank. EG /opt: " -i "/opt" location
 read -e -p "Please specify port not in use by any other service to open the HAPI FHIR server on, EG 8090: " -i "8090" port
-read -e -p "If your system uses firewalld (AKA firewall-cmd), would you like me to automatically open the port and reload the firewall? y/n: " -i "n" openfirewall
+read -e -p "If your system uses firewalld (AKA firewall-cmd), would you like to automatically open the port and reload the firewall? y/n: " -i "n" openfirewall
 
 cd "$location"
 
@@ -50,7 +50,7 @@ echo_cmd="echo \"Process already started\""
 check_if_running="$(ps -aux | grep -v \"grep\" | grep \"java -jar $location/hapi-fhir-cli.jar run-server -v dstu3 -p 8090 --allow-external-refs\")"
 output_cmd="if [\"\" != \"$check_if_running\"]; then\n\t$nohup_cmd\nelse\n\techo_cmd\nfi"
 
-echo "$output_cmd"> "$location/start-hapi-fhir-server.sh"
+echo -e "$output_cmd"> "$location/start-hapi-fhir-server.sh"
 
 bash "$location/start-hapi-fhir-server.sh"
 
